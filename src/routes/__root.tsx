2c6c79223829744dcbd1a44dfab9e15a2ac8ad4f
Nnadi Victory <chikamagozi25@gmail.com>
2025-11-09 21:23:34 +0100

    Remove wallet connection success toast feature

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
-import { WalletConnectionToast } from '@/components/wallet-connection-toast';
 
 import '@fontsource/albert-sans/400.css';
 import '@fontsource/albert-sans/500.css';
@@ -77,7 +76,6 @@ function RootComponent() {
 				});
 			}}>
 			<WalletProvider autoConnect>
-				<WalletConnectionToast />
 				<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
 					<RootDocument>
 						<NuqsAdapter>
